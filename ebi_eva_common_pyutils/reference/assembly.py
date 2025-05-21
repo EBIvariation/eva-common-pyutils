5836ce60d57959074be7f96ae801db85afd27e54
tcezard <tcezard@ebi.ac.uk>
2025-05-21 10:22:34 +0100

    Revert back the update option and add docstring

--- ebi_eva_common_pyutils/reference/assembly.py
+++ ebi_eva_common_pyutils/reference/assembly.py
@@ -235,14 +235,17 @@ class NCBIAssembly(AppLogger):
         self.info('Downloading ' + contig_accession)
         urllib.request.urlretrieve(url, output_file)
 
-    def download_or_construct(self, genbank_only=False, overwrite=False, update=False):
-        """First download the assembly report and fasta from the FTP, then append any missing contig from
-        the assembly report to the assembly fasta."""
-        if not update or overwrite:
-            self.download_assembly_report(overwrite)
-            try:
-                self.download_assembly_fasta(overwrite)
-            except:
-                pass
+    def download_or_construct(self, genbank_only=False, overwrite=False):
+        """
+        First download the assembly report and fasta from the FTP, then append any missing contig from
+        the assembly report to the assembly fasta.
+        Setting genbank_only = True ensure that only contigs that have genbank accession will be added to the assembly fasta.
+        Setting overwrite = True delete any existing fasta file and assembly report. then download them again.
+        """
+        self.download_assembly_report(overwrite)
+        try:
+            self.download_assembly_fasta(overwrite)
+        except:
+            pass
         # This will either confirm the presence of all the contig or download any one missing
         self.construct_fasta_from_report(genbank_only)
